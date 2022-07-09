770040de3f0651729dcac48698d042cc4c633bb4
flaviapl <flavia_pala@hotmail.it>
2022-07-09 10:18:16 +0000

    ejercicioac

--- src/app.js
+++ src/app.js
@@ -6,6 +6,33 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function onload() {
+    let random = Math.floor(Math.random() * 3);
+    let random1 = Math.floor(Math.random() * 2);
+    let random2 = Math.floor(Math.random() * 4);
+    let excuse =
+      who[random] +
+      " " +
+      action[random] +
+      " " +
+      what[random1] +
+      " " +
+      when[random2];
+    return excuse;
+  }
+
+  document.querySelector("#button").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = onload();
+  });
 };
